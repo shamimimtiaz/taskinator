a3d255282d15eb0bc4d371f75c66a6f35e29f04e
Shamim Imtiaz <km_si@ymail.com>
2020-12-30 21:17:05 -0500

    Add name, type of task and refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,32 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-console.log(buttonEl);
+var createTaskHandler = function (event) {
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;  //we use square brackets [ ] in a selector, we're trying to select an HTML element(input) by one of its (name) attributes set to a value of "task-name"
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-var createTaskHandler = function() {
-    var taskItemEl = document.createElement("li");
-    taskItemEl.textContent = "This is a new task";
-    tasksToDoEl.appendChild(taskItemEl);
-    taskItemEl.className="task-item";  //for CSS
-}
+  // create list item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
 
-buttonEl.addEventListener ("click",  createTaskHandler);
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
 
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", createTaskHandler);
+
+
+
+
+
+  //listItemEl.textContent = taskNameInput; 
+  
